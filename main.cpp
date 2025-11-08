e3ed4a518cb4793f9d20758b0dafd45ae29f2ad7
sameen02 <2248500948@qq.com>
2025-11-08 17:05:54 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string name = "Sameen";
+	string major = "biomedical engineering";
+	cout <<"姓名"<<name<<endl;
+	cout <<"专业"<<major<<endl;
+	return 0;}
+
---
+++ test-python.py
@@ -0,0 +1,4 @@
+name = "Sameen"
+major = "biomedical enginering"
+print(f"姓名：{name}")
+print(f"专业: {major}")
